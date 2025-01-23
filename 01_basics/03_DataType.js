e0ea8a7e1d042a88180ac6f39b96ddae1245dcb0
khotAkash07 <khotakash5737@gmail.com>
2025-01-23 18:43:59 +0000

    This is new files with changing names of file.

---
+++ 01_basics/03_DataType.js
@@ -0,0 +1,30 @@
+const firstNumber = 199;
+const bigNumber = 2341243124n;
+const isNull = null;
+
+//Symbol datatype
+
+const id = Symbol('122')
+const secId = Symbol('122')
+console.log(typeof(secId)+"  "+typeof(id));
+
+
+//Array DataType
+
+const myArray = ["Akash","Abhi","Shubham"]
+
+const myObject = {
+    name1 : "Akash",
+    age : 23,
+    Address : "Shivpuri"
+}
+console.log(myObject);
+
+//fumction in javaScript
+
+let myFunction = function(){
+    console.log("This is my first function");
+    
+}
+
+// myFunction();
