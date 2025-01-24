ff21d94eec14ed56f7f5fe9d3d25e7a2e4e976a7
khotAkash07 <khotakash5737@gmail.com>
2025-01-24 17:52:46 +0000

    Addding some maths content in 06th file

--- 01_basics/06_Num_And_Maths.js
+++ 01_basics/06_Num_And_Maths.js
@@ -1,16 +1,44 @@
 let number = 12345675;
 
 let num2 = number.toString();
-console.log(num2);
-console.log(typeof num2);
+// console.log(num2);
+// console.log(typeof num2);
 
-console.log(number.toLocaleString('en-IN'))
+// console.log(number.toLocaleString('en-IN'))
 
 let decimalNumber = 1234.19767;
-console.log(decimalNumber.toFixed(2));
+// console.log(decimalNumber.toFixed(2));
 
-console.log(decimalNumber.toPrecision(5))
-console.log(Number.isInteger(decimalNumber));
+// console.log(decimalNumber.toPrecision(5))
+// console.log(Number.isInteger(decimalNumber));
+
+
+//----------------   Maths   ----------------
+
+
+const num = 12.62
+
+// console.log(Math.abs(num)); // change negative value into positive value
+
+// console.log(Math.abs(Math.round(num))); 
+
+// console.log(Math.ceil(num)); // It take greater value 
+
+// console.log(Math.floor(num)); // It take smaller value 
+
+// console.log(Math.floor((Math.random()*10 ))+1); // A randon function take any random value in btn 0 to 1
+
+
+
+let min = 20
+let max = 50
+console.log(Math.floor(Math.random()* (max - min +1))+min);
+
+
+
+
+
+
 
 
 
